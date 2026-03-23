bd2a0a18c327e0beb2a119f87a98632373ed5124
Tringa Hyseni <tringa.hyseni@umib.net>
2026-03-23 20:34:55 +0100

    refactor: remove unused authRoutes and improve server log formatting

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const path = require("path");
 const userRoutes = require("./UI/routes/userRoutes");
 const apartmentRoutes = require("./UI/routes/apartmentRoutes");
 const bookingRoutes = require("./UI/routes/bookingRoutes");
-const authRoutes = require("./UI/routes/authRoutes");
 
 const app = express();
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "UI/frontend")));
+
 app.use("/users", userRoutes);
 app.use("/apartments", apartmentRoutes);
 app.use("/bookings", bookingRoutes);
-app.use("/auth", authRoutes);
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
